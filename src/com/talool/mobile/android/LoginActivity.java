a2e1ea71798540f20f5a89fd341ad3b9af165d68
Cory Zachman <zachmanc@Corys-MacBook-Pro.local>
2013-07-08 22:01:00 -0600

    Initial Android commit

---
+++ src/com/talool/mobile/android/LoginActivity.java
@@ -0,0 +1,23 @@
+package com.talool.mobile.android;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_layout);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
