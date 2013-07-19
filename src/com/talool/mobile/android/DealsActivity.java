5f7fa12137007ced11c9570749994e6900ebd9a3
Chris Lintz <christopher.justin@gmail.com>
2013-07-18 21:56:41 -0600

    Created Application for once initialized components - font-awesome for example

--- src/com/talool/mobile/android/DealsActivity.java
+++ src/com/talool/mobile/android/DealsActivity.java
@@ -2,49 +2,55 @@ package com.talool.mobile.android;
 
 import org.apache.thrift.transport.TTransportException;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.LocationSource;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.model.LatLng;
 import com.talool.api.thrift.Merchant_t;
 import com.talool.mobile.android.util.ThriftHelper;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.widget.EditText;
-
-public class DealsActivity extends Activity {
+public class DealsActivity extends Activity
+{
 	private static ThriftHelper client;
 	private Merchant_t merchant;
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.deals_activity_layout);
 		MapView mapView = (MapView) findViewById(R.id.map);
 		mapView.onCreate(savedInstanceState);
 		String lat = (String) getIntent().getSerializableExtra("Lat");
 		String lon = (String) getIntent().getSerializableExtra("Lon");
-	    try {
+		try
+		{
 			MapsInitializer.initialize(this);
-		} catch (GooglePlayServicesNotAvailableException e1) {
+		}
+		catch (GooglePlayServicesNotAvailableException e1)
+		{
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
 
-		CameraUpdate update = CameraUpdateFactory.newLatLngZoom(new LatLng(Double.valueOf(lat),Double.valueOf(lon)), 15);
+		CameraUpdate update = CameraUpdateFactory.newLatLngZoom(new LatLng(Double.valueOf(lat), Double.valueOf(lon)), 15);
 		mapView.getMap().moveCamera(update);
-		try {
+		try
+		{
 			client = new ThriftHelper();
-		} catch (TTransportException e) {
 		}
+		catch (TTransportException e)
+		{}
 	}
 
 	@Override
-	protected void onDestroy() {
+	protected void onDestroy()
+	{
 		// TODO Auto-generated method stub
 		super.onDestroy();
 		MapView mapView = (MapView) findViewById(R.id.map);
@@ -52,7 +58,8 @@ public class DealsActivity extends Activity {
 	}
 
 	@Override
-	protected void onPause() {
+	protected void onPause()
+	{
 		// TODO Auto-generated method stub
 		super.onPause();
 		MapView mapView = (MapView) findViewById(R.id.map);
@@ -60,7 +67,8 @@ public class DealsActivity extends Activity {
 	}
 
 	@Override
-	protected void onResume() {
+	protected void onResume()
+	{
 		// TODO Auto-generated method stub
 		super.onResume();
 		MapView mapView = (MapView) findViewById(R.id.map);
