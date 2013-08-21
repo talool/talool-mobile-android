ce484e196805774dab674a0bc7f0f9315bd6b88d
Doug McCuen <doug@talool.com>
2013-08-21 10:47:19 -0600

    clean up some imports and remove the extra setting menu on the gift view

--- src/com/talool/mobile/android/activity/GiftActivity.java
+++ src/com/talool/mobile/android/activity/GiftActivity.java
@@ -301,7 +301,7 @@ public class GiftActivity extends Activity
     public boolean onCreateOptionsMenu(Menu menu)
     {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        //getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
