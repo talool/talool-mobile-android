9e61180a0916f0acf7709c6378fac853f3df780d
Bryan Pauk <paukb@colorado.edu>
2013-08-20 21:43:02 -0600

    removed notifications

--- src/com/talool/mobile/android/util/NotificationHelper.java
+++ src/com/talool/mobile/android/util/NotificationHelper.java
@@ -22,9 +22,6 @@ import com.talool.mobile.android.tasks.ActivitySupervisor;
 public class NotificationHelper {
 
     //todo add dynamic messages
-    public static final String NOTIFICATION_MESSAGE = "Received gift \"2 for 1 lunch\" at The Kitchen";
-    private final static String NOTIFICATION_TITLE = "Update Test Message";
-    private int currentNotificationId = 0;
     private int previousCount = 0;
     private ActionBar.Tab notificationTab;
     private Context applicationContext;
@@ -52,34 +49,6 @@ public class NotificationHelper {
         });
     }
 
-    private void newNotification(int notificationId, String notificationTitle, String notificationMessage, int totalNotifications, Object o) {
-        updateNotificationTab(totalNotifications);
-
-        //todo need a better way to distinguish updated notifications from new.
-
-        if (previousCount != totalNotifications){
-            previousCount = totalNotifications;
-            /*if (currentNotificationId >= Integer.MAX_VALUE){
-                currentNotificationId = 0;
-            }
-            else {
-                currentNotificationId ++;
-            } */
-
-            //todo build intent. need to clear previousCount on resume
-            NotificationCompat.Builder builder = new NotificationCompat.Builder(applicationContext)
-                    .setSmallIcon(R.drawable.icon_teal)
-                    .setContentTitle(NOTIFICATION_TITLE)
-                    .setContentText(NOTIFICATION_MESSAGE)
-                    .setNumber(totalNotifications)
-                    .setContentIntent(null);
-
-            // Builds the notification and issues it.
-            notificationManager.notify(currentNotificationId, builder.build());
-        }
-    }
-
-
     public void updateNotificationTab(final int count){
         uiThreadHandler.post(new Runnable() {
             @Override
@@ -102,7 +71,7 @@ public class NotificationHelper {
         @Override
         public void handleNotificationCount(int totalNotifications) {
             if (notificationManager != null && totalNotifications != previousCount){
-                    newNotification(currentNotificationId, NOTIFICATION_TITLE, NOTIFICATION_MESSAGE, totalNotifications, null);
+                updateNotificationTab(totalNotifications);
             }
         }
     };
