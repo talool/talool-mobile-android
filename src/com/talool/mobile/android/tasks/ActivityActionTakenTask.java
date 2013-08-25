48a7f15ccf3f8f4de621c8ef201bde7e693a1742
Chris Lintz <christopher.justin@gmail.com>
2013-08-25 17:09:57 -0600

    activity bug fixes/forced refresh of activity persistence when accept/reject/read message

--- src/com/talool/mobile/android/tasks/ActivityActionTakenTask.java
+++ src/com/talool/mobile/android/tasks/ActivityActionTakenTask.java
@@ -4,12 +4,10 @@ import org.apache.thrift.TException;
 
 import android.content.Context;
 import android.os.AsyncTask;
-import android.util.Log;
 
-import com.google.analytics.tracking.android.EasyTracker;
-import com.google.analytics.tracking.android.MapBuilder;
-import com.google.analytics.tracking.android.StandardExceptionParser;
+import com.talool.api.thrift.Activity_t;
 import com.talool.api.thrift.ServiceException_t;
+import com.talool.mobile.android.persistence.ActivityDao;
 import com.talool.mobile.android.util.TaloolUtil;
 import com.talool.mobile.android.util.ThriftHelper;
 
@@ -23,12 +21,17 @@ public class ActivityActionTakenTask extends AsyncTask<Void, Void, Void>
 	private ThriftHelper client;
 	private String activityId;
 	private Context context;
+	private ActivityDao activityDao;
+	private Activity_t activity;
 
-	public ActivityActionTakenTask(final ThriftHelper client, final String activityId, Context context)
+	public ActivityActionTakenTask(final ThriftHelper client, final String activityId, final Context context,
+			final ActivityDao activityDao, final Activity_t activity)
 	{
 		this.client = client;
 		this.activityId = activityId;
 		this.context = context;
+		this.activityDao = activityDao;
+		this.activity = activity;
 	}
 
 	@Override
@@ -37,18 +40,21 @@ public class ActivityActionTakenTask extends AsyncTask<Void, Void, Void>
 		try
 		{
 			client.client.activityAction(activityId);
+			activity.setActionTaken(true);
+			activityDao.saveActivity(activity);
+			ActivitySupervisor.get().refreshFromPersistence();
 		}
 		catch (ServiceException_t e)
 		{
-			TaloolUtil.sendException(e,context);
+			TaloolUtil.sendException(e, context);
 		}
 		catch (TException e)
 		{
-			TaloolUtil.sendException(e,context);
+			TaloolUtil.sendException(e, context);
 		}
 		catch (Exception e)
 		{
-			TaloolUtil.sendException(e,context);
+			TaloolUtil.sendException(e, context);
 		}
 
 		return null;
--- src/com/talool/mobile/android/util/NotificationHelper.java
+++ src/com/talool/mobile/android/util/NotificationHelper.java
@@ -5,74 +5,84 @@ import android.app.NotificationManager;
 import android.content.Context;
 import android.os.Handler;
 import android.os.Looper;
-import android.support.v4.app.NotificationCompat;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
 import com.talool.mobile.android.R;
 import com.talool.mobile.android.tasks.ActivitySupervisor;
 
-
 /**
- * Created with IntelliJ IDEA.
- * User: bryan
- * Date: 8/18/13
- * Time: 8:24 PM
- * To change this template use File | Settings | File Templates.
+ * Created with IntelliJ IDEA. User: bryan Date: 8/18/13 Time: 8:24 PM To change
+ * this template use File | Settings | File Templates.
  */
-public class NotificationHelper {
+public class NotificationHelper
+{
 
-    //todo add dynamic messages
-    private int previousCount = 0;
-    private ActionBar.Tab notificationTab;
-    private Context applicationContext;
-    private Handler uiThreadHandler;
-    private NotificationManager notificationManager;
+	// todo add dynamic messages
+	private int previousCount = 0;
+	private ActionBar.Tab notificationTab;
+	private Context applicationContext;
+	private Handler uiThreadHandler;
+	private NotificationManager notificationManager;
 
-    public NotificationHelper(ActionBar.Tab notificationTab, Context applicationContext) {
-        this.notificationTab = notificationTab;
-        this.applicationContext = applicationContext;
+	public NotificationHelper(ActionBar.Tab notificationTab, Context applicationContext)
+	{
+		this.notificationTab = notificationTab;
+		this.applicationContext = applicationContext;
 
-        notificationManager = (NotificationManager) applicationContext.getSystemService(Context.NOTIFICATION_SERVICE);
+		notificationManager = (NotificationManager) applicationContext.getSystemService(Context.NOTIFICATION_SERVICE);
 
+		uiThreadHandler = new Handler(Looper.getMainLooper());
 
-        uiThreadHandler = new Handler(Looper.getMainLooper());
+		initializeActivitySupervisor();
+	}
 
-        initializeActivitySupervisor();
-    }
+	private void initializeActivitySupervisor()
+	{
+		uiThreadHandler.post(new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				ActivitySupervisor.createInstance(applicationContext, notificationCallback);
+			}
+		});
+	}
 
-    private void initializeActivitySupervisor() {
-        uiThreadHandler.post(new Runnable() {
-            @Override
-            public void run() {
-                ActivitySupervisor.createInstance( applicationContext, notificationCallback);
-            }
-        });
-    }
+	public void updateNotificationTab(final int count)
+	{
+		uiThreadHandler.post(new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				// todo clean clean clean
+				if (notificationTab != null)
+				{
+					if (count > 0)
+					{
+						((TextView) notificationTab.getCustomView().findViewById(R.id.actionbar_notifcation_textview)).setText(Integer.toString(count));
+						notificationTab.getCustomView().findViewById(R.id.activity_text_count).setVisibility(RelativeLayout.VISIBLE);
+					}
+					else
+					{
+						notificationTab.getCustomView().findViewById(R.id.activity_text_count).setVisibility(RelativeLayout.GONE);
+					}
+				}
+			}
+		});
+	}
 
-    public void updateNotificationTab(final int count){
-        uiThreadHandler.post(new Runnable() {
-            @Override
-            public void run() {
-                //todo clean clean clean
-                if (notificationTab != null){
-                    if (count > 0){
-                        ((TextView)notificationTab.getCustomView().findViewById(R.id.actionbar_notifcation_textview)).setText(Integer.toString(count));
-                        notificationTab.getCustomView().findViewById(R.id.activity_text_count).setVisibility(RelativeLayout.VISIBLE);
-                    }
-                    else {
-                        notificationTab.getCustomView().findViewById(R.id.activity_text_count).setVisibility(RelativeLayout.GONE);
-                    }
-                }
-            }
-        });
-    }
-
-    ActivitySupervisor.NotificationCallback notificationCallback = new ActivitySupervisor.NotificationCallback() {
-        @Override
-        public void handleNotificationCount(int totalNotifications) {
-            if (notificationManager != null && totalNotifications != previousCount){
-                updateNotificationTab(totalNotifications);
-            }
-        }
-    };
+	ActivitySupervisor.NotificationCallback notificationCallback = new ActivitySupervisor.NotificationCallback()
+	{
+		@Override
+		public void handleNotificationCount(int totalNotifications)
+		{
+			if (notificationManager != null && totalNotifications != previousCount)
+			{
+				updateNotificationTab(totalNotifications);
+				previousCount = totalNotifications;
+			}
+		}
+	};
 }
