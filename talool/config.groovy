b4fa4a84a7e72a93efd0889b33d78cbc501b72e9
Chris Lintz <christopher.justin@gmail.com>
2015-02-15 15:16:42 -0700

    config.groovy changes for external configs

---
+++ talool/config.groovy
@@ -0,0 +1,17 @@
+environments {
+    dev_chamber {
+        serverName = 'http://localhost:9090'
+    }
+
+    prod_chamber {
+        serverName = 'http://localhost:9090'
+    }
+
+    dev_talool {
+        serverName = 'http://testserver'
+    }
+
+    prod_talool {
+        serverName = 'http://www.mrhaki.com'
+    }
+}
